6cf953d83005c0cd09f02f422af71f5ceb3819a1
Koushal S Modi <124229657+koushalsmodi@users.noreply.github.com>
2024-07-26 20:00:03 -0700

    Update credit.py

--- credit.py
+++ credit.py
@@ -1,18 +1,5 @@
 from cs50 import get_int
-def main():
-    while True:
-        # Get input for credit card number
-        credit_card = get_int('Number: ')
-        if credit_card >= 0:
-            break  # Continue asking for valid input until a non-negative number is entered
 
-    if check_validity(credit_card):
-        print_card_brand(credit_card)
-
-    else:
-        print('Invalid')
-
-    
 def main():
     while True:
         # Get input for credit card number
