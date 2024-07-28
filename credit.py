1d37da489a4305041231864b6853f5ec0ba20ec8
Koushal S Modi <124229657+koushalsmodi@users.noreply.github.com>
2024-07-27 21:12:44 -0700

    Update credit.py

--- credit.py
+++ credit.py
@@ -18,6 +18,7 @@ def main():
 
 def check_validity(credit_card_number):
     reversed_digits = [int(digit) for digit in str(credit_card_number)][::-1]
+    total = sum(get_luhn_digit(digit, index) for index, digit in enumerate(reverse_digits))
     
 
 def check_validity(credit_card_number):
